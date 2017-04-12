a82ee24983e29ed069b08ce0140e28e2aea3456d
antonio <antonio.borrelli88@gmail.com>
2017-04-12 14:50:28 +0200

    commit

--- dot2metric/src/costanti/Costanti.java
+++ dot2metric/src/costanti/Costanti.java
@@ -3,12 +3,13 @@ package costanti;
 public abstract class Costanti {
 
 	//PATH DIRECTORY
-	public static String PATH_DIRECTORY_INPUT	="html";
+	public static String PATH_DIRECTORY_INPUT	="input";
+//	public static String PATH_DIRECTORY_INPUT	="html";
 	public static String PATH_DIRECTORY_OUTPUT	="output";
 		
 	//PATH FILE
-//	public static String PATH_FILE_INPUT="input01.dot";
+	public static String PATH_FILE_INPUT="example.dot";
 //	public static String PATH_FILE_INPUT="input02.dot";
-	public static String PATH_FILE_INPUT="input03.dot";
+//	public static String PATH_FILE_INPUT="classcom_1_1jaeksoft_1_1pojodbc_1_1_query_ab296f2c345981790bbf8b7fe8c337d9c_cgraph.dot";
 //	public static String PATH_FILE_INPUT="input04.dot";
 }
--- dot2metric/src/test/HelloTest.java
+++ dot2metric/src/test/HelloTest.java
@@ -8,17 +8,22 @@ import java.util.ArrayList;
 import costanti.Costanti;
 import grammatica.DotGrammar;
 import grammatica.DotGrammarTokenManager;
+import grammatica.ParseException;
 import grammatica.Token;
 
 public class HelloTest {
 
 	
 	
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, ParseException {
 		// TODO Auto-generated method stub
 
-//		FileInputStream fis = new FileInputStream(new File(Costanti.PATH_DIRECTORY_INPUT+"/"+Costanti.PATH_FILE_INPUT));
-//		DotGrammar dot = new DotGrammar(fis);
+		ArrayList<String> lista_metodi_chiamati = new ArrayList<>();
+		FileInputStream fis = new FileInputStream(new File(Costanti.PATH_DIRECTORY_INPUT+"/"+Costanti.PATH_FILE_INPUT));
+		DotGrammar dot = new DotGrammar(fis);
+		System.out.println("NOME METODO : " + dot.test());
+		
+		
 //		Token token = null;
 //		do{
 //			token = dot.getNextToken();
@@ -27,19 +32,19 @@ public class HelloTest {
 //		}while(token.kind != 0);
 		
 		
-		ArrayList<String> listafile = readInput(Costanti.PATH_DIRECTORY_INPUT);
-		
-		for (String nome_file : listafile) {
-			System.out.println("\n\n ******************************** FILE: " + nome_file + "************************************************ \n\n");
-			FileInputStream fis = new FileInputStream(new File(Costanti.PATH_DIRECTORY_INPUT+"/"+nome_file));
-			DotGrammar dot = new DotGrammar(fis);
-			Token token = null;
-			do{
-				token = dot.getNextToken();
-				String type = DotGrammarTokenManager.tokenImage[token.kind];
-				System.out.println("Value: \t" + token.image + "\t Line_inizio: \t" + token.beginLine + "\t Line_fine: \t" + token.endLine + "\t Column_inizio: \t" + token.beginColumn + "\t Column_fine: \t" + token.endColumn + "\t Type: \t" + type +"\n");
-			}while(token.kind != 0);
-		}
+//		ArrayList<String> listafile = readInput(Costanti.PATH_DIRECTORY_INPUT);
+//		
+//		for (String nome_file : listafile) {
+//			System.out.println("\n\n ******************************** FILE: " + nome_file + "************************************************ \n\n");
+//			FileInputStream fis = new FileInputStream(new File(Costanti.PATH_DIRECTORY_INPUT+"/"+nome_file));
+//			DotGrammar dot = new DotGrammar(fis);
+//			Token token = null;
+//			do{
+//				token = dot.getNextToken();
+//				String type = DotGrammarTokenManager.tokenImage[token.kind];
+//				System.out.println("Value: \t" + token.image + "\t Line_inizio: \t" + token.beginLine + "\t Line_fine: \t" + token.endLine + "\t Column_inizio: \t" + token.beginColumn + "\t Column_fine: \t" + token.endColumn + "\t Type: \t" + type +"\n");
+//			}while(token.kind != 0);
+//		}
 		
 	}
 
